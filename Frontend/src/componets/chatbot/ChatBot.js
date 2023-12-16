fc231054f2644b843d9ea44d7e587a0b51eef648
Phuc040502 <phucminhhoang0405@gmail.com>
2023-12-16 20:22:18 +0700

    init project

---
+++ Frontend/src/componets/chatbot/ChatBot.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Chatbot.css'
+
+const ChatBot = () => {
+  return (
+    <div className='chatbox_ctn'>
+      <div className='header_ctn'>
+        <p>Chatbot tự động</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default ChatBot
